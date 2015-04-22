7ce8d1251c70d18be080d36c9e01dd7ac443a96f
sohan1 <shn619@gmail.com>
2015-04-22 15:05:38 +0530

    Done
    Done

--- index.php
+++ index.php
@@ -20,31 +20,31 @@
     <script src="lib/ionic/js/ionic.bundle.min.js"></script>
 
     <script>
-            var isMobile = {
-                Android: function () {
-                    return navigator.userAgent.match(/Android/i);
-                },
-                BlackBerry: function () {
-                    return navigator.userAgent.match(/BlackBerry/i);
-                },
-                iOS: function () {
-                    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-                },
-                Opera: function () {
-                    return navigator.userAgent.match(/Opera Mini/i);
-                },
-                Windows: function () {
-                    return navigator.userAgent.match(/IEMobile/i);
-                },
-                any: function () {
-                    return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-                }
-            };
-
-            if (!isMobile.any()) {
-                window.location = "http://www.magicmirror.in"+window.location.hash;
+        var isMobile = {
+            Android: function () {
+                return navigator.userAgent.match(/Android/i);
+            },
+            BlackBerry: function () {
+                return navigator.userAgent.match(/BlackBerry/i);
+            },
+            iOS: function () {
+                return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+            },
+            Opera: function () {
+                return navigator.userAgent.match(/Opera Mini/i);
+            },
+            Windows: function () {
+                return navigator.userAgent.match(/IEMobile/i);
+            },
+            any: function () {
+                return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
             }
-        </script>
+        };
+
+        if (!isMobile.any()) {
+            window.location = "http://www.magicmirror.in" + window.location.hash;
+        }
+    </script>
 
     <!-- cordova script (this will be a 404 during development) -->
     <!--    <script src="cordova.js"></script>-->
